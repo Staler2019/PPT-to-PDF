c1a815622873cc5e5effde3c0901bbe66bcb7b93
Staler2019 <abcd121234@yahoo.com.tw>
2022-04-07 10:14:51 +0800

    22.4.7 modify code

--- README.md
+++ README.md
@@ -11,3 +11,7 @@
 
 1. `python3 ppt2Pdf.py`
 2. key in your full destination of folder
+
+## TODO
+1. change only one file
+2. select which file to change
--- ppt2Pdf.py
+++ ppt2Pdf.py
@@ -1,6 +1,11 @@
 import glob
+import os
+import tqdm
+import win32com.client
+from pathlib import Path
 
 ppttoPDF = 32  # I forget what is this
+PDFLOCATION = "./PDF"
 
 
 def getFolder():
@@ -9,53 +14,50 @@ Please input the full destination folder(ex. C:\Users\abcd1\Class\Embedded Syste
     >"""
     srcDir = input(srcStr)
     print()
-    # r'C:\Users\abcd1\Documents\My Files\大二下\星期四 計算機組織\Handouts'
-    return srcDir
+    return srcDir  # r'C:\Users\abcd1\Documents\My Files\大二下\星期四 計算機組織\Handouts'
+
+
+def save2PDF(file, endwith):
+    savingfile = pdfPath + "\\" + file.split("\\")[-1]
+
+    try:
+        deck = powerpoint.Presentations.Open(file)
+        deck.SaveAs(savingfile[: -len(endwith)], ppttoPDF)
+        deck.Close()
+        # delete file
+        # os.remove(file)
+
+    except BaseException as e:  # catch pywintypes.error(inherit from BaseException)
+        print(e.args)
+        pass
 
 
 def pptChanger(files1, files2):
-    import os
-    import tqdm
-    import win32com.client
-
-    powerpoint = win32com.client.Dispatch("Powerpoint.Application")
     for f in tqdm.tqdm(files1):  # ppt
         print()
         print(f)
 
-        try:
-            # kernel
-            deck = powerpoint.Presentations.Open(f)
-            deck.SaveAs(f[:-4], ppttoPDF)
-            deck.Close()
-            os.remove(f)
-
-        except BaseException as e:  # catch pywintypes.error(inherit from BaseException)
-            print(e.args)
-            pass
+        save2PDF(f, ".ppt")
 
     for f in tqdm.tqdm(files2):  # pptx
         print()
         print(f)
 
-        try:
-            # kernel
-            deck = powerpoint.Presentations.Open(f)
-            deck.SaveAs(f[:-5], ppttoPDF)
-            deck.Close()
-            os.remove(f)
+        save2PDF(f, ".pptx")
 
-        except BaseException as e:  # to catch pywintypes.error
-            print(e.args)
-            pass
 
-    powerpoint.Quit()
+def createPDFfolder(dir):
+    Path(dir).mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == "__main__":
     srcPath = getFolder()
+    pdfPath = srcPath + "\\" + PDFLOCATION[2:]
+    createPDFfolder(pdfPath)
 
     files1 = [f for f in glob.glob(srcPath + "**/*.ppt", recursive=True)]  # ppt
     files2 = [f for f in glob.glob(srcPath + "**/*.pptx", recursive=True)]  # pptx
 
+    powerpoint = win32com.client.Dispatch("Powerpoint.Application")
     pptChanger(files1, files2)
+    powerpoint.Quit()
