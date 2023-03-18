d71b206944c551ec50982ca10cb342914024c29c
Staler2019 <abcde121234@gmail.com>
2023-03-18 22:13:56 +0800

    Fix error err: only convert depth=1 's files

--- ppt2Pdf.py
+++ ppt2Pdf.py
@@ -1,11 +1,13 @@
-import glob
 import os
-import tqdm
-import win32com.client
 from pathlib import Path
+import win32com.client
 
-ppttoPDF = 32  # I forget what is this
-PDFLOCATION = "./PDF"
+import glob
+import tqdm
+
+
+ppttoPDF = 32
+PDF_LOC = "PDF"
 
 
 def getFolder():
@@ -17,47 +19,42 @@ Please input the full destination folder(ex. C:\Users\abcd1\Class\Embedded Syste
     return srcDir  # r'C:\Users\abcd1\Documents\My Files\大二下\星期四 計算機組織\Handouts'
 
 
-def save2PDF(file, endwith):
-    savingfile = pdfPath + "\\" + file.split("\\")[-1]
+def save2pdf(f):
+    fileName = os.path.splitext(f.split("\\")[-1])[0] + ".pdf"
+    fileSaveDir = os.path.join(os.path.dirname(f), PDF_LOC)
+    saveFileName =  os.path.join(fileSaveDir, fileName)
+
+    Path(fileSaveDir).mkdir(parents=True, exist_ok=True)
+
+    print("to", saveFileName)
 
     try:
-        deck = powerpoint.Presentations.Open(file)
-        deck.SaveAs(savingfile[: -len(endwith)], ppttoPDF)
+        # Load presentation
+        deck = powerpoint.Presentations.Open(f)
+        # Convert PPTX to PDF
+        deck.SaveAs(saveFileName, ppttoPDF)
         deck.Close()
+
         # delete file
         # os.remove(file)
 
     except BaseException as e:  # catch pywintypes.error(inherit from BaseException)
         print(e.args)
-        pass
-
-
-def pptChanger(files1, files2):
-    for f in tqdm.tqdm(files1):  # ppt
-        print()
-        print(f)
-
-        save2PDF(f, ".ppt")
-
-    for f in tqdm.tqdm(files2):  # pptx
-        print()
-        print(f)
-
-        save2PDF(f, ".pptx")
-
-
-def createPDFfolder(dir):
-    Path(dir).mkdir(parents=True, exist_ok=True)
 
 
 if __name__ == "__main__":
     srcPath = getFolder()
-    pdfPath = srcPath + "\\" + PDFLOCATION[2:]
-    createPDFfolder(pdfPath)
 
-    files1 = [f for f in glob.glob(srcPath + "**/*.ppt", recursive=True)]  # ppt
-    files2 = [f for f in glob.glob(srcPath + "**/*.pptx", recursive=True)]  # pptx
+    types = ("/**/*.ppt", "/**/*.pptx")
+    files = []
+    for ftype in types:
+        files.extend(glob.glob(srcPath + ftype, recursive=True))
 
     powerpoint = win32com.client.Dispatch("Powerpoint.Application")
-    pptChanger(files1, files2)
+
+    for f in tqdm.tqdm(files):
+        print()
+        print(f)
+        save2pdf(f)
+
     powerpoint.Quit()
